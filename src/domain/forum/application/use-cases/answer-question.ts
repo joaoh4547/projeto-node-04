eb246a3ee60e1a92df59b41b509c95f8c77b8c4f
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-29 19:49:21 -0300

    Capitulo 2 Parte 2

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -1,5 +1,6 @@
-import { UniqueEntityId } from "../../core/entities/value-objects/unique-entity-id";
-import { Answer } from "../entities/answer";
+
+import { UniqueEntityId } from "@/core/entities/value-objects/unique-entity-id";
+import { Answer } from "../../enterprise/entities/answer";
 import { AnswersRepository } from "../repositories/answers-repository";
 
 export interface AnswerQuestionUseCaseInputParams {
