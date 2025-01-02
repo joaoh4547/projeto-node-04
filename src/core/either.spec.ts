e2d5fe6646b1c699e872b079cca4e3dbef18fc74
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-01 21:41:56 -0300

    Capitulo 3 Parte 2

---
+++ src/core/either.spec.ts
@@ -0,0 +1,27 @@
+
+import { Either, left, right } from "./either";
+
+function doSomething(x: boolean): Either<string, number> {
+    if (x) {
+        return right(10);
+    }
+    return left("error");
+}
+
+test("success result", async () => {
+    const result = doSomething(true);
+
+    if(result.isRight()) {
+        console.log(result.value);
+    }
+
+
+    expect(result.isRight()).toBe(true);
+    expect(result.isLeft()).toBe(false);
+});
+
+test("error result", async () => {
+    const result = doSomething(false);
+    expect(result.isLeft()).toBe(true);
+    expect(result.isRight()).toBe(false);
+});
